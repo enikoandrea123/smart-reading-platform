b47f77d8e75e37b69032252266e2cc22ad4c286c
enikoandrea123 <enikoandrea@mailbox.unideb.hu>
2025-02-17 15:16:03 +0100

    add new page: Favourites

--- smart-reading-platform/frontend/src/App.js
+++ smart-reading-platform/frontend/src/App.js
@@ -14,6 +14,8 @@ import ForgotPassword from './components/ForgotPassword';
 import Explore from './components/Explore';
 import Profile from './components/Profile';
 import BookDetail from './components/BookDetail';
+import Favourites from './components/Favourites';
+
 
 function App() {
   return (
@@ -30,6 +32,7 @@ function App() {
         <Route path="/explore" element={<Explore/>} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/book/:bookId" element={<BookDetail />} />
+        <Route path="/book/:bookId" element={<Favourites />} />
     </Routes>
     <Footer/>
     </Router>
